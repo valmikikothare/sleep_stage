a05633b103e8d399f21c634f68fea1fadcafac2b
Valmiki Kothare <valmiki@mit.edu>
2023-09-22 14:37:39 -0400

    First commit

---
+++ jupyter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J jupyter
+#SBATCH -t 08:00:00
+#SBATCH --gres=gpu:1 
+#SBATCH -n 1
+#SBATCH --constraint=rocky8
+#SBATCH --mem 20G
+#SBATCH -o jupyter.out
+
+source ~/.bashrc
+conda activate pytorch
+unset XDG_RUNTIME_DIR
+
+port=2141
+
+jupyter lab --ip=0.0.0.0 --port=${port} --no-browser --NotebookApp.allow_origin='*' --NotebookApp.port_retries=0
+
+# If lose connection from server
+# lsof -ti:2141 | xargs kill -9
